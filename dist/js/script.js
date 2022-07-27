c55a5a143aa0e0f4ee403e18569163428817bd6d
Kostya <kostya.mikhalkov@bk.ru>
2022-07-27 17:30:57 +0300

    alivio

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,23 @@ const counters = document.querySelectorAll('.skills__percent'),
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+$(document).ready(function() {
+	$('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
+		if (document.form.name.value == '' || document.form.phone.value == '' ) {
+			valid = false;
+			return valid;
+		}
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$('.js-overlay-thank-you').fadeIn();
+			$(this).find('input').val('');
+			$('#form').trigger('reset');
+		});
+		return false;
+	});
+});
+
